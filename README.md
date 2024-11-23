55c77517e84edc3bace670e26f1f2b9c23efb8e1
Mitchel Baker <github.trolling181@passmail.net>
2024-11-23 13:32:28 -0800

    update readme instructions, add requirements.txt, update file structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# finance-aggregator
+
+## Setup virtualenv and install dependencies
+
+pip3 install virtualenv
+virtualenv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+
+## Run development server
+
+python3 src/server.py
--- src/server.py
+++ src/server.py
@@ -1,12 +1,12 @@
 import json
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@server.route('/', methods=['GET'])
 def get_root():
   arr = ["endpoint works as expected"]
   return jsonify(arr)
 
 if __name__ == '__main__':
-  app.run(port=3101)
+  server.run(port=3101)
