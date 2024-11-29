faa1d84e149f93d70e580923f471e50606b43881
Mitchel Baker <github.trolling181@passmail.net>
2024-11-28 22:40:55 -0800

    create route to get plaid access token

--- src/routes/plaid.py
+++ src/routes/plaid.py
@@ -1,13 +1,17 @@
-import plaid
 from plaid.api import plaid_api
-from flask import Blueprint, jsonify, request
+from plaid.model.sandbox_public_token_create_request import SandboxPublicTokenCreateRequest
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.products import Products
+from flask import Blueprint
 
+import plaid
+import json
 import config
 
 plaid_blueprint = Blueprint('plaid', __name__)
 
 configuration = plaid.Configuration(
-  host=config.PLAID_HOST,
+  host=plaid.Environment.Sandbox,
   api_key={
     'clientId': config.PLAID_CLIENT_ID,
     'secret': config.PLAID_SECRET
@@ -16,19 +20,17 @@ configuration = plaid.Configuration(
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
 
-@plaid_blueprint.route('/get_public_token', methods=['GET'])
-def get_public_token():
-  print("in get public token")
+@plaid_blueprint.route('/get_access_token', methods=['GET'])
+def get_access_token():
   FIRST_PLATYPUS_BANK = 'ins_109508'
   pt_request = SandboxPublicTokenCreateRequest(
     institution_id = FIRST_PLATYPUS_BANK,
     initial_products = [Products('transactions')]
   )
   pt_response = client.sandbox_public_token_create(pt_request)
-  # The generated public_token can now be
-  # exchanged for an access_token
-  exchange_request = ItemPublicTokenExchangeRequest(
-      public_token=pt_response['public_token']
-  )
+  public_token = pt_response["public_token"]
+
+  exchange_request = ItemPublicTokenExchangeRequest(public_token=public_token)
   exchange_response = client.item_public_token_exchange(exchange_request)
-  return exchange_response
+  json_string = json.dumps(exchange_response.to_dict(), default=str)
+  return json_string
