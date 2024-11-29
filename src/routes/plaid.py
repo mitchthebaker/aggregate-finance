59eb71b67900337e12c6798364e42fb09b0741c9
Mitchel Baker <github.trolling181@passmail.net>
2024-11-28 21:37:19 -0800

    plaid initial route setup for public token

--- src/routes/plaid.py
+++ src/routes/plaid.py
@@ -1,15 +1,34 @@
 import plaid
 from plaid.api import plaid_api
+from flask import Blueprint, jsonify, request
 
 import config
 
+plaid_blueprint = Blueprint('plaid', __name__)
+
 configuration = plaid.Configuration(
-  host=plaid.Environment[config.PLAID_HOST]
+  host=config.PLAID_HOST,
   api_key={
-    'clientId': config.PLAID_CLIENT_ID
+    'clientId': config.PLAID_CLIENT_ID,
     'secret': config.PLAID_SECRET
   }
 )
-
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
+
+@plaid_blueprint.route('/get_public_token', methods=['GET'])
+def get_public_token():
+  print("in get public token")
+  FIRST_PLATYPUS_BANK = 'ins_109508'
+  pt_request = SandboxPublicTokenCreateRequest(
+    institution_id = FIRST_PLATYPUS_BANK,
+    initial_products = [Products('transactions')]
+  )
+  pt_response = client.sandbox_public_token_create(pt_request)
+  # The generated public_token can now be
+  # exchanged for an access_token
+  exchange_request = ItemPublicTokenExchangeRequest(
+      public_token=pt_response['public_token']
+  )
+  exchange_response = client.item_public_token_exchange(exchange_request)
+  return exchange_response
--- src/server.py
+++ src/server.py
@@ -1,13 +1,15 @@
 import json
 from flask import Flask, jsonify, request
 from flasgger import Swagger
+from routes import plaid_blueprint
 
 import config
-import routes
 
 server = Flask(__name__)
 server.debug = config.DEBUG
 Swagger(server)
+
+server.register_blueprint(plaid_blueprint, url_prefix='/plaid')
 
 @server.route('/', methods=['GET'])
 def get_root():
